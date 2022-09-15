5782527b339942c3f0e2bb9a1f14f2eee1719052
Artem Kirilchuk <101064292+embraceddd@users.noreply.github.com>
2022-09-15 20:53:17 +0300

    Initial commit

---
+++ src/main/java/com/example/someproject/SomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.someproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/someproject/SomeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.someproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
